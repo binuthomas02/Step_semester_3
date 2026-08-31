0e6830fa87b778c6365802fa3806696263117f79
THOMAS <jesusthesaviour0205@gmail.com>
2026-08-31 10:13:27 +0530

    assignment_problems

---
+++ session_1/assignment_problems/SeatChecker.java
@@ -0,0 +1,34 @@
+import java.util.Scanner;
+
+class SeatChecker
+{
+    static void checkDuplicateSeats(int[] seatNumbers)
+    {
+        boolean duplicate = false;
+
+        for(int i = 0; i < seatNumbers.length; i++)
+        {
+            for(int j = i + 1; j < seatNumbers.length; j++)
+            {
+                if(seatNumbers[i] == seatNumbers[j])
+                {
+                    System.out.println("Duplicate Seat Number Found: " + seatNumbers[i]);
+                    duplicate = true;
+                    break;
+                }
+            }
+        }
+
+        if(!duplicate)
+        {
+            System.out.println("No Duplicate Seats Found");
+        }
+    }
+
+    public static void main(String[] args)
+    {
+        int[] seatNumbers = {101, 102, 103, 102, 105};
+
+        checkDuplicateSeats(seatNumbers);
+    }
+}
